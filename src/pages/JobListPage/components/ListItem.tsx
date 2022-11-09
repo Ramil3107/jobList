17bf5a406cd122090e6c077d995f30584438c12b
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-10 01:36:30 +0300

    create list item and main content components

---
+++ src/pages/JobListPage/components/ListItem.tsx
@@ -0,0 +1,15 @@
+import { MainContent } from "./MainContent"
+
+export const ListItem = () => {
+    return (
+
+        // Item List Box
+        <div className="flex w-[70%] max-w-[1400px] h-[185px] my-2 py-[24px] px-[16px] bg-white rounded-[9px] shadow-md"> 
+
+         {/* Avatar + Headline + Department name + Adress */}
+            <MainContent />
+
+        
+        </div>
+    )
+}
---
+++ src/pages/JobListPage/components/MainContent.tsx
@@ -0,0 +1,14 @@
+
+export const MainContent = () => {
+    return (
+        <div className="flex w-full">
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" className="w-[85px] h-[85px] rounded-full" />
+                
+                <div className="flex flex-col w-[60%] h-[130px] ml-[26px] border">
+                    <h1>Hello</h1>
+                    <p>Descrp</p>
+                    <p>Adress</p>
+                </div>
+        </div>
+    )
+}
