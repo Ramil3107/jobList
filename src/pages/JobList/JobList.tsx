9f58b71978b50878942c71d636c4239c3691415e
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-10 00:54:45 +0300

    create job list item box

--- src/pages/JobList/JobList.tsx
+++ src/pages/JobList/JobList.tsx
@@ -1,6 +1,9 @@
 
 export const JobList = () => {
+
   return (
-    <div>JobList Page</div>
+    <div className="flex flex-col items-center w-full min-h-screen bg-[#F5F5F5] py-7">
+      <div className="w-[70%] h-[120px] my-2 bg-white rounded-[9px] shadow-md"></div>
+    </div>
   )
 }
