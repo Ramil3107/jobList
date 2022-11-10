4af872424808d6a63417a12e4aa24ba1299678ad
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-10 22:51:31 +0300

    create pagination component

--- src/pages/JobListPage/JobListPage.tsx
+++ src/pages/JobListPage/JobListPage.tsx
@@ -1,11 +1,17 @@
 import { ListItem } from "./components/ListItem"
+import { Pagination } from "./components/Pagination"
 
 export const JobListPage = () => {
 
   return (
-    <div className="flex flex-col items-center w-full min-h-screen bg-[#F5F5F5] py-7">
+    // Wrapper
+    <div className="flex flex-col items-center w-full min-h-screen bg-[#F5F5F5] py-7 relative pb-[100px]">
 
+      {/* Job List Items */}
       <ListItem />
+
+      {/* Pagination */}
+      <Pagination />
 
     </div>
   )
