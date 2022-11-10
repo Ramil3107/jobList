e4a429982d6862e691429b61217fb55b152f54b6
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-10 21:07:17 +0300

    fix width for main contant

--- src/pages/JobListPage/components/MainContent.tsx
+++ src/pages/JobListPage/components/MainContent.tsx
@@ -2,13 +2,14 @@ import { MdLocationOn } from "react-icons/md"
 
 export const MainContent = () => {
     return (
-        <div className="flex w-full">
+        <div className="flex w-[70%] border">
 
             <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" className="w-[85px] h-[85px] rounded-full" />
 
-            <div className="flex flex-col justify-between w-[65%] h-[130px] ml-[26px] border">
+            <div className="flex flex-col justify-between w-full h-[130px] ml-[26px] ">
                 <h1 className="text-[20px] text-[#3A4562] font-bold leading-[25px] mb-[8px]">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores ab maxime aspernatur!
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Maiores ab maxime!
                 </h1>
                 <p className="text-[#878D9D] text-[16px] font-normal mb-[8px]">
                     Department name • Allgemeines Krankenhaus der Stadt Wien - AKH
